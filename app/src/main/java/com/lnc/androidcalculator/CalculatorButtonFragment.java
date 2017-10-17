9e48ed8b8c38757f660792f8669caf0fdc592d72
Lucas Cramer <LucasC@iqmetrix.com>
2017-10-17 14:55:48 -0700

    Button works

--- app/src/main/java/com/lnc/androidcalculator/CalculatorButtonFragment.java
+++ app/src/main/java/com/lnc/androidcalculator/CalculatorButtonFragment.java
@@ -10,16 +10,22 @@ import android.widget.Button;
 
 public class CalculatorButtonFragment extends Fragment implements View.OnClickListener {
 
+    Button _button;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.calculator_button_fragment, container, false);
+        View view = inflater.inflate(R.layout.calculator_button_fragment, container, false);
+
+        _button = view.findViewById(R.id.calculator_button);
+        _button.setOnClickListener(this);
+
+        return view;
     }
 
     @Override
     public void onClick(View view) {
         ICalculatorFragmentCoordinator activity = (ICalculatorFragmentCoordinator) getActivity();
-        Button button = view.findViewById(R.id.calculator_button);
-        activity.onButtonClicked(button.getText().toString());
+        activity.onButtonClicked(_button.getText().toString());
     }
 }
